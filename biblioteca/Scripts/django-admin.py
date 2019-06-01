49d8abd1ecd2c03e3065edc018fab806eb53170f
nestor <adamspalomino@gmail.com>
2019-06-01 18:41:37 -0500

    starting the project

---
+++ biblioteca/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\nesto\onedrive\escritorio\django\biblioteca\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
